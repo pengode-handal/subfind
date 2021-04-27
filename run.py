2f8ff0b02e4d830f92a2c16c31f90d1d5ba9a4ac
Gura Chan <77978775+BabwaXgura@users.noreply.github.com>
2021-04-27 05:51:01 -0400

    Add files via upload

---
+++ run.py
@@ -0,0 +1,73 @@
+
+import json
+import requests
+import time
+import os 
+from os import system as sistem
+import argparse
+from argparse import *
+from requests import *
+sistem("clear")
+m='\033[31;1m'
+h='\033[32;1m'
+k='\033[33;1m'
+p='\033[37;1m'
+b='\033[34;1m'
+n='\033[00;1m'
+bg='\033[47;1m'
+
+print("""\n\n
+      {}    ╔═══╗  ╔╗ ╔═══╗         {}{}{}Gura chan{}{}
+          ║╔═╗║  ║║ ╚╗╔╗║           
+          ║╚══╦╗╔╣╚═╗║║║╠══╦╗╔╦══╦╦══╗
+          ╚══╗║║║║╔╗║║║║║╔╗║╚╝║╔╗╠╣╔╗║
+          ║╚═╝║╚╝║╚╝╠╝╚╝║╚╝║║║║╔╗║║║║║
+          ╚═══╩══╩══╩═══╩══╩╩╩╩╝╚╩╩╝╚╝
+{}{}==================Information=================
+||Tools     : SubDomain Finder              ||
+||Author    : Babwa/Guraa-chan              ||
+||help      : CLI Python3 System            ||
+==================Information=================\n\n """.format(b, n, b, bg, n, b, n, m))
+
+def main():
+  
+  parser = ArgumentParser()
+  parser.add_argument("-d", "--domain", help="domain (dont use http/https)")
+  parser.add_argument("-s", "--save", help="input the filename")
+  parser.add_argument("-v", "--version", action="version", version="SubDomain Scanner Tool V1.1.0")
+  args = parser.parse_args()
+  
+  def ngk():
+    print("[+] Ya udh kalo ga mau di simpen")
+  
+  pika = ("##########RESULT###########")
+  nma = ("")
+  
+  def logger(pika):
+    file = open(str(nma) + ".txt", "a")
+    file.write(str(pika))
+    file.write("\n")    
+    file.close()
+    
+    
+ 
+  
+  nma = args.save
+  logger(pika)
+    
+  data = args.domain
+  api = "http://jamet1337.ml/api/subdo.php?url=" + data
+  mulai = time.time()
+  ngeload = requests.get(api)
+  gura = json.loads(ngeload.text)
+  pika = gura['hasil']
+  logger(pika)
+  print("##########{}RESULT{}{}###########".format(b, n, m))
+  print(h + pika)
+  stop = time.time()
+  be = int(stop - mulai)
+  print("\n{}butuh waktu {} detik untuk selesai".format(k, be))
+  sistem("rm -rf None.txt")
+  
+if __name__ == '__main__':
+  main()
